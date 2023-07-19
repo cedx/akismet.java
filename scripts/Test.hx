ac9b11030f82ed3f35ea697d2539fef3095047a7
Cédric Belin <cedric@belin.io>
2023-07-19 13:19:41 +0200

    Update the build system

--- scripts/Test.hx
+++ scripts/Test.hx
@@ -1,10 +1,11 @@
+using StringTools;
+
 /** Runs the test suite. **/
 function main() {
+	final pkg = "io.belin.akismet";
 	Sys.command("lix Build --debug");
-
-	final pattern = "io/belin/akismet/*.java";
 	Tools.setClassPath();
-	Sys.command('javac -d bin -g -Xlint:all,-processing test/$pattern');
+	Sys.command('javac -d bin -g -Xlint:all,-processing test/${pkg.replace(".", "/")}/*.java');
 
 	final exitCode = Sys.command("java org.junit.platform.console.ConsoleLauncher --select-package=io.belin.akismet");
 	if (exitCode != 0) Sys.exit(exitCode);
