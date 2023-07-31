3d301e5f818023b793ba8e37f0b6c7515f563759
Cédric Belin <cedric@belin.io>
2023-07-31 17:12:25 +0200

    Add the unit tests of the `Author` class [skip ci]

---
+++ test/io/belin/akismet/AuthorTest.java
@@ -0,0 +1,38 @@
+package io.belin.akismet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.UnknownHostException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests the features of the {@see Author} class.
+ */
+@DisplayName("Author")
+final class AuthorTest {
+
+	@Test
+	@DisplayName("toMap()")
+	void toMap() throws UnknownHostException {
+		// It should return only the IP address with a newly created instance.
+		var map = new Author(InetAddress.getByName("127.0.0.1")).toMap();
+		assertEquals(1, map.size());
+		assertEquals("127.0.0.1", map.get("user_ip"));
+
+		// It should return a non-empty map with a initialized instance.
+		var author = new Author(InetAddress.getByName("192.168.0.1"), "Cédric Belin", "cedric@belin.io", URI.create("https://belin.io"));
+		author.userAgent = "Mozilla/5.0";
+
+		map = author.toMap();
+		assertEquals(5, map.size());
+		assertEquals("Cédric Belin", map.get("comment_author"));
+		assertEquals("cedric@belin.io", map.get("comment_author_email"));
+		assertEquals("https://belin.io", map.get("comment_author_url"));
+		assertEquals("Mozilla/5.0", map.get("user_agent"));
+		assertEquals("192.168.0.1", map.get("user_ip"));
+	}
+}
