ae3a176fe71620ff4f5c6207db30973d8ac6edd1
Cédric Belin <cedric@belin.io>
2023-07-31 15:38:42 +0200

    Add the `Blog` class

---
+++ src/io/belin/akismet/Blog.java
@@ -0,0 +1,64 @@
+package io.belin.akismet;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Represents the front page or home URL transmitted when making requests.
+ */
+public class Blog {
+
+	/**
+	 * The character encoding for the values included in comments.
+	 */
+	public Charset charset;
+
+	/**
+	 * The languages in use on the blog or site, in ISO 639-1 format.
+	 */
+	public List<String> languages;
+
+	/**
+	 * The blog or site URL.
+	 */
+	public URI url;
+
+	/**
+	 * Creates a new blog.
+	 * @param url The blog or site URL.
+	 */
+	public Blog(URI url) {
+		this(url, null, null);
+	}
+
+	/**
+	 * Creates a new blog.
+	 * @param url The blog or site URL.
+	 * @param charset The character encoding for the values included in comments.
+	 * @param languages The languages in use on the blog or site, in ISO 639-1 format.
+	 */
+	public Blog(URI url, Charset charset, List<String> languages) {
+		this.charset = charset;
+		this.languages = new ArrayList<>(Objects.requireNonNullElse(languages, Collections.emptyList()));
+		this.url = Objects.requireNonNull(url);
+	}
+
+	/**
+	 * Converts this object to a map.
+	 * @return \stdClass The map in JSON format corresponding to this object.
+	 */
+	public Map<String, String> toMap() {
+		var map = new HashMap<String, String>();
+		map.put("blog", url.toString());
+		if (charset != null) map.put("blog_charset", charset.toString());
+		if (!languages.isEmpty()) map.put("blog_lang", languages.stream().collect(Collectors.joining(",")));
+		return map;
+	}
+}
