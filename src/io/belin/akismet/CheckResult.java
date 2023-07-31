6527615e8d523e156f0a1d1ca816cbfe40acfe68
Cédric Belin <cedric@belin.io>
2023-07-31 15:37:57 +0200

    Add the `CheckResult` enum

---
+++ src/io/belin/akismet/CheckResult.java
@@ -0,0 +1,22 @@
+package io.belin.akismet;
+
+/**
+ * Specifies the result of a comment check.
+ */
+public enum CheckResult {
+
+	/**
+	 * The comment is not a spam (i.e. a ham).
+	 */
+	Ham,
+
+	/**
+	 * The comment is a spam.
+	 */
+	Spam,
+
+	/**
+	 * The comment is a pervasive spam (i.e. it can be safely discarded).
+	 */
+	PervasiveSpam
+}
